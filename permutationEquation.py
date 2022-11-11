257ccbadc1018e6fe5c226e4c4d5a05b20cd83ad
cavala <cavala@gmail.com>
2022-11-11 10:25:52 -0300

    permutationEquation
    Foi mais difícil entender do que realmente fazer.

---
+++ permutationEquation.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    result = []    
+    for index, val in enumerate(p):
+        position = index+1
+        result.append(p.index(p.index(position)+1)+1)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
